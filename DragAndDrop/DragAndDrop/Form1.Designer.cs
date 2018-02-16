197440582742f0e3ed6dbac36f651860d4b5872a
FranzAlexander <alexbalcombe4@gmail.com>
2018-02-16 12:23:19 +0800

    Add files via upload

---
+++ DragAndDrop/DragAndDrop/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DragAndDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mediaPlayerLbl = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mediaPlayerLbl
+            // 
+            this.mediaPlayerLbl.AutoSize = true;
+            this.mediaPlayerLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediaPlayerLbl.Location = new System.Drawing.Point(486, 9);
+            this.mediaPlayerLbl.Name = "mediaPlayerLbl";
+            this.mediaPlayerLbl.Size = new System.Drawing.Size(171, 31);
+            this.mediaPlayerLbl.TabIndex = 0;
+            this.mediaPlayerLbl.Text = "Media Player";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 43);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1013, 596);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1031, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Video File";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1035, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 71);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Drag In Here";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 651);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.mediaPlayerLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mediaPlayerLbl;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DragAndDrop/DragAndDrop/Form1.cs
@@ -0,0 +1,90 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        TextWriterTraceListener debugWriter = new TextWriterTraceListener("trace.log");
+
+        /// <summary>
+        /// This is a nested class.
+        /// </summary>
+        class videoInfo
+        {
+            public string videoUrl { get; set; }
+        }
+
+        videoInfo videoData = new videoInfo();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This method is triggered when the user releases the drag when over the panel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            //Setting the data type.
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //storing the file into a files array.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                //Storing the video string into the video object.
+                videoData.videoUrl = files.FirstOrDefault();
+
+                //Making sure that the video string is not empty.
+                if (!string.IsNullOrWhiteSpace(videoData.videoUrl))
+                {
+                    //Getting the url of the video and playing it in the media player.
+                    axWindowsMediaPlayer1.URL = videoData.videoUrl;
+                    debugWriter.WriteLine($"{videoData.videoUrl.ToString()} Log Success");
+                    debugWriter.Flush();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is for when the mouse is dragging something and it enters over the panel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            //The drag and drop effects.
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+                
+            }
+        }
+    }
+}
